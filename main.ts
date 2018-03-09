aea243140fc54f98fd1693526328f59b04cff859
Wongsakorn Manoruean <wongsakornm58@email.nu.ac.th>
2018-03-09 17:21:00 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 
-/**
- * Generated class for the Test0Page page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-main',
